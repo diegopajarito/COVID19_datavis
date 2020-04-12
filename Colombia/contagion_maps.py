3e43b7e8d8c7927f8ee8d567a31531aa7540a890
diegopajarito <diegopajarito@gmail.com>
2020-04-11 23:37:27 -0500

    Updated for Colombia April 11.

--- Colombia/contagion_maps.py
+++ Colombia/contagion_maps.py
@@ -1,5 +1,6 @@
 import geopandas
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import getData
@@ -28,44 +29,21 @@ print('Matched boundaries: %i. Unmatched boundaries: %i' % (matched, unmatched))
 
 # Plot Individual maps per region
 title = 'COVID-19 cases near %s airport(s)'
-# divider = make_axes_locatable(ax)
-#cax = divider.append_axes("top", size="5%", pad=0.1)
 airport_areas = [['Bogotá', (-76, 3.4, -73, 5.65)],
                  ['Medellín', (-77, 5.7, -75, 7.4)],
                  ['Cali', (-77, 3.3, -75, 4.8)],
                  ['North-East', (-74, 6.9, -72, 8.4)],
                  ['Caribbean Coast', (-76, 9.7, -72.5, 12.325)]]
 geodata = geodata[geodata['cases'] > 0]
-# for region in airport_areas:
-#     fig, ax = plt.subplots(1, 1)
-#     ax.tick_params(labelbottom=False, labelleft=False, left=False, bottom=False)
-#     ax.set_facecolor('black')
-#     base = boundaries.plot(ax=ax, color='black', edgecolor='gray', linewidth=0.2)
-#     boundaries_region.plot(ax=base, color='None', edgecolor='gray', linewidth=0.8)
-#     geodata.plot(ax=base, column='cases', cmap='OrRd', legend=True,
-#                  legend_kwds={'label': "Confirmed cases per municipality", 'orientation': "horizontal"})
-#     limits = region[1]
-#     xlim = (limits[0], limits[2])
-#     ylim = (limits[1], limits[3])
-#     airports[airports['internac'] == '1'].plot(ax=base, marker='o', color='purple', markersize=8)
-#     plt.title(title % region[0])
-#     plt.xlim(xlim)
-#     plt.ylim(ylim)
-#     plt.show()
-#     plt.savefig('maps/map_airports_%s.png' % region[0])
-#     plt.close()
-
-print('Done with individual plots')
-
-
+geodata['cases_log'] = np.log10(geodata['cases'])
 # Single plot with region maps
-title = 'COVID-19 confirmed cases & airports in Colombia (Bogotá)'
+title = 'COVID-19 confirmed cases & int. airports in Colombia (Bogotá)'
 fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=(7, 9))
 gs = ax1.get_gridspec()
 ax1.remove()
 ax2.remove()
 ax1 = fig.add_subplot(gs[0, 0:])
-airport_areas = [['  ', (-77, 3.5, -72, 5.8), ax1],
+airport_areas = [['  ', (-77, 3.3, -72, 5.4), ax1],
                  ['Medellín', (-76.8, 5.6, -74.8, 7.1), ax3],
                  ['Cali', (-77, 3.3, -75, 4.8), ax4],
                  ['Bucaramanga - Cúcuta', (-74, 6.9, -72, 8.4), ax5],
@@ -78,11 +56,10 @@ for region in airport_areas:
     base = boundaries.plot(ax=ax, color='black', edgecolor='gray', linewidth=0.2)
     boundaries_region.plot(ax=base, color='None', edgecolor='gray', linewidth=0.8)
     if ax == ax1:
-        geodata.plot(ax=base, column='cases', cmap='OrRd', legend=True, legend_kwds={'label': "Cases"})
+        geodata.plot(ax=base, column='cases_log', cmap='OrRd', legend=True, legend_kwds={'label': "Log(x) Cases"})
     else:
-        boundaries_region.plot(ax=base, color='None', edgecolor='gray', linewidth=0.8)
-    geodata.plot(ax=base, column='cases', cmap='OrRd', legend=False)
-    airports[airports['internac'] == '1'].plot(ax=base, marker='o', color='purple', markersize=8)
+        geodata.plot(ax=base, column='cases_log', cmap='OrRd', legend=False)
+    airports[airports['internac'] == '1'].plot(ax=base, marker='o', color='cyan', legend=True, markersize=8, alpha=0.7)
     ax.tick_params(labelbottom=False, labelleft=False, left=False, bottom=False)
     ax.set_facecolor('black')
     ax.set_title(region[0])
