da843a0e128576e801a229fa3596f12d0f72732b
diegopajarito <diegopajarito@gmail.com>
2020-04-17 21:57:15 -0500

    Updated data for Colombia, April 17 2020

--- Colombia/getData.py
+++ Colombia/getData.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import urllib.request
-ins_co_link = "https://e.infogram.com/3a4c9dd3-fbb8-4f59-8d82-eb1edc01604b?src=embed#"
+ins_co_link = "https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD"
 ins_co_file = 'data/Casos.csv'
-names = ['id_case', 'date', 'city', 'region', 'treatment', 'age', 'sex', 'origin', 'origin_country']
+names = ['id_case', 'date', 'city', 'region', 'treatment', 'age', 'sex', 'origin', 'origin_country', 'date_death',
+         'date_recovered']
 
 
 def get_ins_data():
@@ -13,6 +14,7 @@ def get_ins_data():
 def get_ins_data_file():
     ins_co_data = pd.read_csv(ins_co_file)
     ins_co_data.columns = names
-    ins_co_data['date'] = pd.to_datetime(ins_co_data['date'], dayfirst=True)
+    ins_co_data['date_str'] = ins_co_data['date']
+    ins_co_data['date'] = pd.to_datetime(ins_co_data['date'], dayfirst=True, errors='coerce')
     return ins_co_data
 
